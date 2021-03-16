333f57d56caf688b9d49009dbeaa7dc9a0d722a6
Quentin SL <56533657+Quentin-SL@users.noreply.github.com>
2021-03-16 19:43:37 +0400

    [Feat](register) commit register page

---
+++ pages/register.vue
@@ -0,0 +1,147 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-card>
+                <v-card-title class="title">
+                    Register
+                </v-card-title>
+            <v-form class="reg_form"
+            ref="form"
+            v-model="valid"
+            lazy-validation
+            >
+
+            <v-row>
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="Firstname"
+                    :rules="firstnameRules"
+                    label="First name"
+                    required
+                    outlined
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="Lastname"
+                    :rules="lastnameRules"
+                    label="Last name"
+                    required
+                    outlined
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="Username"
+                    :rules="usernameRules"
+                    label="Username"
+                    required
+                    outlined
+                ></v-text-field>
+                </v-col>
+            </v-row>
+
+                <v-text-field
+                    v-model="Email"
+                    :rules="emailRules"
+                    label="Email"
+                    required
+                    outlined
+                
+                    ></v-text-field>    
+
+                    <v-text-field
+                    v-model="Password"
+                    :rules="passwordRules"
+                    label="Password"
+                    required
+                    outlined
+                    ></v-text-field>
+
+                    <v-text-field
+                    v-model="ConfirmPassword"
+                    :rules="confirmpasswordRules"
+                    label="Confirm Password"
+                    required
+                    outlined
+                    ></v-text-field>
+
+                    <v-btn
+                    color="success"
+                    class="mr-4"
+                    v-on:click="validate()"
+                    >
+                    Validate
+                    </v-btn>
+            </v-form>
+            <v-card-text>
+            <p>Not registered yet ? <nuxt-link to="/register">Register here</nuxt-link></p>
+            </v-card-text>
+            </v-card>
+        </v-main>
+    </v-app>
+
+</template>
+
+<style>
+.reg_form{
+    margin-left: 10px;
+    margin-right: 10px;
+}
+.title{
+    margin-left: 45%;
+    margin-bottom: 15px;
+}
+</style>
+
+<script>
+export default {
+    data(){
+        return {
+            Firstname: '',
+            Lastname: '',
+            Username: '',
+            Email: '',
+            Password: '',
+            ConfirmPassword: '',
+      
+            firstnameRules: [
+                v => !!v || 'First name is required'
+            ],
+            lastnameRules: [
+                v => !!v || 'Last name is required'
+            ],
+            usernameRules: [
+            v => !!v || 'Username is required',
+            ],
+            emailRules: [
+                v => !!v || 'Email is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            passwordRules: [
+                v => !!v || 'Password is required',
+            ],
+        confirmpasswordRules: [
+                v => v === this.Password || 'Password is not the same'
+            ],
+
+            }
+        },
+    methods : {
+            validate(){
+                this.$router.push("login")
+            }
+        }
+    }
+</script>
