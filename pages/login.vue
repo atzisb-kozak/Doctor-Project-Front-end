c9a81657a77e778377a6256548b970b063421e16
atzisb-kozak <alkergoat@gmail.com>
2021-06-01 23:14:53 +0200

    [Feat](login) remove layout + modify login card

--- pages/login.vue
+++ pages/login.vue
@@ -1,60 +1,80 @@
 <template>
   <v-app>
-      
     <v-main>
-        
-      <v-container fluid>
-          
+      <v-container fill-height fluid>
         <v-card 
-          class="card" 
+          class="card rounded-xl mx-auto justify-center"
           dark
           width="500px"
           > 
-            
-          <v-card-title class="title">
-            Connexion
-          </v-card-title>
-            
-          <v-form 
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-card-title class="mt-2 mb-2">
+              Connexion
+            </v-card-title>
+          </v-row>
+          <v-form
             class="log_form"
             ref="form"
             v-model="valid"
             lazy-validation
+          >
+            <v-row
+              align="center"
+              class="ml-2 mr-2"
             >
-
-            <v-text-field
-              v-model="Username"
-              :rules="usernameRules"
-              label="Pseudo"
+              <v-text-field
+                v-model="Username"
+                :rules="usernameRules"
+                class="rounded-pill"
+                label="Pseudo"
+                required
+                outlined
+              >
+              </v-text-field>
+            </v-row>
+            <v-row
+              align="center"
+              class="ml-2 mr-2"
+            >
+              <v-text-field
+              v-model="Password"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="passwordRules"
+              :type="show ? 'text' : 'password'"
+              class="rounded-pill"
+              label="Mot de passe"
               required
               outlined
+              @click:append="show = !show"
+              >
+              </v-text-field>
+            </v-row>
+            <v-row
+              align="center"
+              justify="center"
             >
-            </v-text-field>    
-              
-            <v-text-field
-            v-model="Password"
-            :rules="passwordRules"
-            label="Mot de passe"
-            required
-            outlined
-            >
-            </v-text-field>
-              
-            <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-            >
-              Validate
-            </v-btn>
-
+              <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4 rounded-pill"
+              @click="validate"
+              >
+                Validate
+              </v-btn>
+            </v-row>
           </v-form>
-            
           <v-card-text>
-            <p>Pas encore inscrit ? <nuxt-link to="/register">Inscrivez-vous ici</nuxt-link></p>
+            <v-row
+              align="center"
+              justify="center"
+              class="mt-2"
+            >
+              <p>Pas encore inscrit ? <nuxt-link to="/register">Inscrivez-vous ici</nuxt-link></p>
+            </v-row>
           </v-card-text>
-
         </v-card>
       </v-container>
     </v-main>
@@ -62,13 +82,10 @@
 </template>
 
 <style scoped>
+
 .log_form{
     margin-left: 1rem;
     margin-right: 1rem;
-}
-.title{
-    margin-left: 36.5%;
-    margin-bottom: 1.2rem;
 }
 .card{
   margin-left: 32.5%;
@@ -78,7 +95,7 @@
 <script>
 export default {
   data: () => ({
-
+    show: false,
     Username: '',
     usernameRules: [
       v => !!v || 'Username is required',
@@ -88,11 +105,11 @@ export default {
       v => !!v || 'Password is required',
     ]
   }),
-      
   methods: {
     validate () {
       this.$refs.form.validate();
     }
-  }
+  },
+  layout: 'nolayout'
 };
 </script>
